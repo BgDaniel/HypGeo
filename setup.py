d966fec0a9e06c3820122471cabba388fc467916
Berger, Daniel <Daniel.Berger@L-P-A.com>
2019-11-09 16:11:24 +0100

    add correct package names in setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/BgDaniel/hpygeo",
-    packages=setuptools.find_packages(),
+    packages=['hypgeo', 'hypgeo.helpers'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
